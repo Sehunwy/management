4e3ab3ca6efb9d3e110ebb88b1544b4caf0c7be0
wangyanwy <wy18483680819@163.com>
2019-03-28 17:24:39 +0800

    添加新闻

--- assets/js/commonFunction.js
+++ assets/js/commonFunction.js
@@ -6,6 +6,7 @@ function fileClick(fileChoose, fileName) {
         $('#' + fileName).val(files[0].name);
     });
 }
+
 <!--获取上传文件名称结束-->
 
 // 取消输入框的值开始
@@ -15,4 +16,11 @@ function cancelTitle() {
         textContent[i].value = "";
     }
 }
+
 // 取消输入框的值结束
+
+function showMaxImg(obj) {
+    var src = $(obj).attr("src");
+    $("#imgModal").find("#imgshow").html("<img src='" + src + "' class='carousel-inner img-responsive img-rounded' data-dismiss='modal'>");
+    $("#imgModal").modal('show');
+}
